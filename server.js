4df7b383e1c934312d56739da563bd432f2fb6e7
moozhan <moozhansh@gmail.com>
2024-04-15 23:15:47 +0100

    back to before

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
