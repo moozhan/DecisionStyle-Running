a14b466eabbe35733fe6b19cae1ece89a416ad00
moozhan <moozhansh@gmail.com>
2024-04-21 18:10:38 +0100

    redirect to games after logging in

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 
 const app = express();
-
-// Set EJS as the view engine
 app.set("view engine", "ejs");
-
-// Set the views directory
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 app.use(bodyParser.json());
@@ -26,7 +22,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('trust proxy', 1); // Trust first proxy
 app.use(cors({
-  origin: ['https://lazy-puce-tortoise-yoke.cyclic.app', 'https://moozhan.github.io', 'https://dev-backend.d4id81j7108zr.amplifyapp.com', 'https://7rh93fhc7e.execute-api.eu-central-1.amazonaws.com'],
+  origin: [
+    'https://lazy-puce-tortoise-yoke.cyclic.app',
+    'https://moozhan.github.io',
+    'https://dev-backend.d4id81j7108zr.amplifyapp.com',
+    'https://7rh93fhc7e.execute-api.eu-central-1.amazonaws.com'
+  ],
   credentials: true
 }));
 app.use(cookieParser());
@@ -41,49 +42,35 @@ app.use(session({
   }),
   cookie: {
     httpOnly: true,
-    secure: true, // Should be true in production when using HTTPS
+    secure: true,
     sameSite: 'None'
   }
 }));
 
 app.use(auth({
   issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
-  baseURL: 'https://gleaming-sarong-crab.cyclic.app', // Make sure this is the actual URL from where your app is served in production
+  baseURL: process.env.BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
   secret: process.env.SESSION_SECRET,
   authRequired: false,
   auth0Logout: true,
-  idpLogout: true,
-  postLoginRedirectUri: 'https://gleaming-sarong-crab.cyclic.app/callback', // Redirect to /games after login
-
+  redirectUriPath: '/callback'
 }));
 
-
-// DB Config
 const db = process.env.DB_CONNECTION;
-const options = {
-  serverSelectionTimeoutMS: 5000
-};
-
+const options = { serverSelectionTimeoutMS: 5000 };
 mongoose.connect(db, options)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-//================================================== All pages 
-app.get('/', (req, res) => {
-  res.render('index.ejs');
-});
+//================================================== All pages
+app.get('/', (req, res) => res.render('index.ejs'));
 
-app.get('/about', (req, res) => {
-  res.render('about.ejs');
-});
+app.get('/about', (req, res) => res.render('about.ejs'));
 
 app.get('/callback', (req, res) => {
-  // Handle the authentication response here
-  // You can redirect based on the user's intended destination or other logic
-  const returnTo = req.session.returnTo || '/games';
-  delete req.session.returnTo;
-  res.redirect(returnTo);
+  // Once Auth0 has authenticated the user, redirect to /games
+  res.redirect('/games');
 });
 
 app.get('/games/indecision', requiresAuth(), (req, res) => {
@@ -93,9 +80,10 @@ app.get('/games/indecision', requiresAuth(), (req, res) => {
 app.post('/games/indecision', requiresAuth(), async (req, res) => {
   try {
     const indecision = [
-      req.body.zero, req.body.one, req.body.two, req.body.three, req.body.four, 
-      req.body.five, req.body.six, req.body.seven, req.body.eight, req.body.nine, 
-      req.body.ten, req.body.eleven, req.body.twelve, req.body.thirteen, req.body.fourteen
+      req.body.zero, req.body.one, req.body.two, req.body.three,
+      req.body.four, req.body.five, req.body.six, req.body.seven,
+      req.body.eight, req.body.nine, req.body.ten, req.body.eleven,
+      req.body.twelve, req.body.thirteen, req.body.fourteen
     ];
     const id = req.oidc.user.sub;
     await User.updateOne({ auth0Id: id }, { $push: { "indecision": indecision } });
@@ -118,7 +106,7 @@ app.get('/user', requiresAuth(), (req, res) => {
   });
 });
 
-app.get('/logout', requiresAuth(), (req, res) => {
+app.get('/logout', (req, res) => {
   req.oidc.logout();
   res.redirect('/');
 });
