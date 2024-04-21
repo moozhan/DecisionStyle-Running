0a3f7bc41ab231e1b04edad0ce7e06056cb81ad8
moozhan <moozhansh@gmail.com>
2024-04-21 21:03:00 +0100

    redirect to games after logging in

--- server.js
+++ server.js
@@ -1,150 +1,199 @@
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+  require('dotenv').config()
 }
 
 const express = require('express');
-const mongoose = require('mongoose');
-const MongoStore = require('connect-mongo');
 const session = require('express-session');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const mongoose = require('mongoose');
+// const gameRoutes = require('./routes/gameRoutes');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
 const path = require('path');
-const { auth, requiresAuth } = require('express-openid-connect');
+const { requiresAuth } = require('express-openid-connect');
 const User = require('./models/user');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+// Set EJS as the view engine
 app.set("view engine", "ejs");
+
+// Set the views directory
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('trust proxy', 1);
+
+app.set('trust proxy', 1); // Trust first proxy
 app.use(cors({
-  origin: [
-    'https://lazy-puce-tortoise-yoke.cyclic.app',
-    'https://moozhan.github.io',
-    'https://dev-backend.d4id81j7108zr.amplifyapp.com',
-    'https://7rh93fhc7e.execute-api.eu-central-1.amazonaws.com'
-  ],
+  origin: ['https://lazy-puce-tortoise-yoke.cyclic.app', 'https://moozhan.github.io', 'https://dev-backend.d4id81j7108zr.amplifyapp.com', 'https://7rh93fhc7e.execute-api.eu-central-1.amazonaws.com'], // Update with the location of your HTML file
   credentials: true
 }));
 app.use(cookieParser());
+
+
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true,
-  store: MongoStore.create({
-    mongoUrl: process.env.DB_CONNECTION
-  }),
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',  // Ensure use of HTTPS in production
-    sameSite: 'None'  // Adjust according to your cross-origin needs
+    secure: true, // Should be true in production when using HTTPS
+    sameSite: 'None' // Can be strict or lax depending on your requirements
   }
 }));
 
-app.use(auth({
-  issuerBaseURL: `https://${process.env.AUTH0_DOMAIN}`,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.AUTH0_CLIENT_ID,
-  clientSecret: process.env.AUTH0_CLIENT_SECRET,
-  secret: process.env.SESSION_SECRET,
-  authRequired: false,
-  auth0Logout: true,
-  afterCallback: (req, res, session) => {
-    // Directly redirect to post-login to handle user ID storage
-    res.redirect('/post-login');
-  }
-}));
 
+// DB Config
 const db = process.env.DB_CONNECTION;
-mongoose.connect(db)
+const options = {
+  serverSelectionTimeoutMS: 5000 // Shorten the timeout to fail faster if not connected
+};
+// Connect to MongoDB
+mongoose
+  .connect(db, options)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-  // Middleware to store user ID if not exists
-  async function storeUserId(req, res, next) {
-    if (req.oidc && req.oidc.user) {
-      const userSub = req.oidc.user.sub;
-      try {
-        const existingUser = await User.findOne({ auth0Id: userSub });
-        if (!existingUser) {
-          const newUser = new User({ auth0Id: userSub });
-          await newUser.save();
-          console.log('New user created with ID:', userSub);
-        }
-        // Only call next if no response has been sent
-        if (!res.headersSent) {
-          next();
-        }
-      } catch (err) {
-        console.error("Database operation failed:", err);
-        if (!res.headersSent) {
-          next(err); // Pass error to error handling middleware
-        }
-      }
-    } else {
-      if (!res.headersSent) {
-        next(new Error("User data not available"));
-      }
-    }
+
+passport.use(new Auth0Strategy({
+  domain: process.env.AUTH0_DOMAIN,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  clientSecret: process.env.AUTH0_CLIENT_SECRET,
+  callbackURL: process.env.AUTH0_CALLBACK_URL
+},
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    return done(null, profile);
   }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//================================================== all the pages 
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs');
+});
 
 
-  app.get('/callback', requiresAuth(), (req, res) => {
-    // Check for proper session handling, log session details
-    console.log("Session info:", req.oidc.user);
-  
-    // Ensure only one response is sent
-    if (!res.headersSent) {
-      res.redirect('/post-login');
+app.get('/games/indecision', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render('indecision.ejs');
+  } else {
+    res.status(401).json({ error: 'User is not authenticated' });
+  }
+});
+
+app.post('/games/indecision',  (req, res) => {
+  if (req.isAuthenticated()) {
+    const indecision = {
+      0: req.body.zero,
+      1: req.body.one,
+      2: req.body.two,
+      3: req.body.three,
+      4: req.body.four,
+      5: req.body.five,
+      6: req.body.six,
+      7: req.body.seven,
+      8: req.body.eight,
+      9: req.body.nine,
+      10: req.body.ten,
+      11: req.body.eleven,
+      12: req.body.twelve,
+      13: req.body.thirteen,
+      14: req.body.fourteen
     }
+    const id = req.user.id;
+    User.updateOne({ auth0Id: id }, {$push: {"indecision": indecision}});
+    res.render('/games', {indecision: 'done'});
+  } else {
+    res.status(401).json({ error: 'User is not authenticated' });
+  }
+});
+
+app.get('/games', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render('games.ejs');
+  } else {
+    res.status(401).json({ error: 'User is not authenticated' });
+  }
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+// User data endpoint
+app.get('/user', (req, res) => {
+  if (req.isAuthenticated()) {
+    res.json({
+      id: req.user.id,
+      name: req.user.displayName,
+      emails: req.user.emails
+    });
+  } else {
+    res.status(401).json({ error: 'User is not authenticated' });
+  }
+});
+
+
+
+//============================================== All Auth0 Routes
+// Auth0 login route
+app.get('/login', passport.authenticate('auth0', {
+  scope: 'openid email profile'
+}), (req, res) => {
+  res.redirect('/callback');
+});
+
+// Auth0 callback route
+app.get('/callback', passport.authenticate('auth0', { failureRedirect: '/login' }), (req, res) => {
+  const newUser = new User({
+    auth0Id: req.user.id,
   });
-// Route to handle redirection after login and store user ID
-app.get('/post-login', requiresAuth(), storeUserId, (req, res) => {
+
+  newUser.save()
+    .then(user => res.json(user))
+    .catch(err => console.log(err));
+    
   res.redirect('/games');
 });
-app.get('/', (req, res) => res.render('index.ejs'));
-app.get('/about', (req, res) => res.render('about.ejs'));
-
-app.get('/games/indecision', requiresAuth(), (req, res) => res.render('indecision.ejs'));
-
-app.post('/games/indecision', requiresAuth(), async (req, res) => {
-  try {
-    const indecision = [
-      req.body.zero, req.body.one, req.body.two, req.body.three,
-      req.body.four, req.body.five, req.body.six, req.body.seven,
-      req.body.eight, req.body.nine, req.body.ten, req.body.eleven,
-      req.body.twelve, req.body.thirteen, req.body.fourteen
-    ];
-    const id = req.oidc.user.sub;
-    await User.updateOne({ auth0Id: id }, { $push: { "indecision": indecision } });
-    res.redirect('/games');
-  } catch (err) {
-    console.error("Error updating user:", err);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-app.get('/games', requiresAuth(), (req, res) => res.render('games.ejs'));
-
-app.get('/user', requiresAuth(), (req, res) => {
-  res.json({
-    id: req.oidc.user.sub,
-    name: req.oidc.user.name,
-    emails: req.oidc.user.email
-  });
-});
-
-app.get('/login', (req, res) => req.oidc.login({ returnTo: '/post-login' }));
 
 app.get('/logout', (req, res) => {
-  req.oidc.logout({
-    returnTo: '/',
+  console.log("Logging out user...");
+  req.logout(function (err) {
+    if (err) { return next(err); }
+
+    let returnTo = `${req.protocol}://${req.get('host')}`;
+    const port = req.socket.localPort;
+    if (port !== undefined && port !== 80 && port !== 443) {
+      returnTo = process.env.NODE_ENV === 'production' ? returnTo : `${returnTo}:${port}`;
+    }
+
+    const logoutURL = new URL(`https://${process.env.AUTH0_DOMAIN}/v2/logout`);
+    const searchString = new URLSearchParams({
+      client_id: process.env.AUTH0_CLIENT_ID,
+      returnTo: returnTo
+    });
+    logoutURL.search = searchString.toString();
+
+    console.log("Redirecting to Auth0 logout URL:", logoutURL.toString());
+    res.redirect(logoutURL.toString());
   });
 });
 
